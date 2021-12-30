47de1d35cb03fd43a440f975f67a40e94a7fb31f
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-12-29 18:11:04 -0800

    completed the sidebar for linkedin-clone

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,28 +3,44 @@ import React from 'react';
 import './Sidebar.css';
 
 function Sidebar() {
-  return <div className="sidebar">
-    <div className="sidebar__top">
-      <img src="" alt="" />
-      <Avatar className="sidebar__avatar" />
-      <h2>Abdulrahman Mohammed</h2>
-      <h4>abdulrahman48811@gmail.com</h4>
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
     </div>
+  );
 
-    <div className="sidebar__stats">
-      <div className="sidebar__stat">
-        <p>Who viewed you</p>
-        <p className="sidebar__statNumber">2.543</p>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://lh3.googleusercontent.com/X5INodl4_gHa9PYylwJhNfE4Mynt9HezcWGwShNK1hJCXzCtoDSZxICTfDibkzhmjMQiYi53qG11X3bhCD3tKFME8iqXbYbNxB77NHxO7eonusDBd1-32w8VjL_2dHueQe9uTSr_=w2400"
+          alt="" />
+        <Avatar className="sidebar__avatar" />
+        <h2>Abdulrahman Mohammed</h2>
+        <h4>abdulrahman48811@gmail.com</h4>
       </div>
-      <div className="sidebar__stat">
-        <p>Views on post</p>
-        <p className="sidebar__statNumber">2.448</p>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2.543</p>
         </div>
-    </div>
-    <div className="sidebar__bottom">
-      <p>Recent</p>
-    </div>
-  </div> 
-}
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2.448</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")} 
+      </div>
+    </div> 
+  )
+};
 
 export default Sidebar
